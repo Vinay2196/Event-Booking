fb61b0812b9b37f3dc8752a2645fd6cb5eb316d8
Vinay <vinaydhobi96@gmail.com>
2025-04-15 16:59:45 +0530

    UPDATE: Add role when register

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,19 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, role } = req.body;
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "Email already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hashedPassword });
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
